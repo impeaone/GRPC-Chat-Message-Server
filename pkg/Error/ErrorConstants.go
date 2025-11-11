7743a0d504bc5a20d27bcbeecd948691a25154a9
Vlad <golikv070@gmail.com>
2025-11-11 14:45:07 +0300

    Message server

---
+++ pkg/Error/ErrorConstants.go
@@ -0,0 +1,6 @@
+package Error
+
+const (
+	LogFileDoesNotOpen  = "Log-file does not open"
+	LogFileDoesNotWrite = "Log-file does not write logs"
+)
---
+++ pkg/config/config.go
@@ -0,0 +1,34 @@
+package config
+
+import (
+	consts "GrpcMessangerMsgServer/pkg/constants"
+	"gopkg.in/yaml.v3"
+	"os"
+	"runtime"
+)
+
+type Config struct {
+	Port      int    `yaml:"Port"`
+	IPAddress string `yaml:"IPAddress"`
+}
+
+func ReadConfig() (*Config, error) {
+	var ConfPath string
+	if runtime.GOOS == "windows" {
+		ConfPath = consts.ConfigPathWindows
+	} else if runtime.GOOS == "linux" {
+		ConfPath = consts.ConfigPathLinux
+	} else {
+		ConfPath = consts.ConfigPathLinux // Ну там, дааа, пока что так
+	}
+	bytes, err := os.ReadFile(ConfPath)
+	if err != nil {
+		return nil, err
+	}
+	var config Config
+	errUnmarshal := yaml.Unmarshal(bytes, &config)
+	if errUnmarshal != nil {
+		return nil, errUnmarshal
+	}
+	return &config, nil
+}
