5958dc07a590792cac52e031a9ba49fdbab7f512
impeaone <151428260+impeaone@users.noreply.github.com>
2026-01-29 19:23:24 +0300

    Update main.go

--- cmd/run/main.go
+++ cmd/run/main.go
@@ -8,18 +8,15 @@ import (
 )
 
 func main() {
-	//TODO: какую-нибудь бд для действий пользователей или т.д. хз кароче
 
 	logs := logger.NewLog()
-	StoragePath := "" // TODO: хз что это должно было быть
+	StoragePath := ""
 
-	//Файл с конфигом, но там маловато, конечно
 	config, err := config2.ReadConfig()
 	if err != nil {
 		logs.Error(fmt.Sprintf("Reading config file error: %v", err), logger.GetPlace())
 		return
 	}
-	//логи все глубже
 	application := app.NewApp(config.Port, StoragePath, logs)
 	// стартуем
 	application.GRPCServer.MustRun()
